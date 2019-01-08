bcf584f6a15a252881ce3d9396b199d512353a9a
rasmuslaaneiktkh <rasmus.laane@khk.ee>
2019-01-08 13:23:14 +0200

    modified theme `Storefront` version 2.4.2

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -18,11 +18,7 @@
 		<div class="col-full">
 
 			
-			
-			<?php
-				echo 'Loe ka meie e-poe <a href="http://rasmuslaane.ikt.khk.ee/wordpress5/e-poe-kasutustingimused/">kasutustingimusi</a>.';
-			?>
-			
+						
 			<?php
 			/**
 			 * Functions hooked in to storefront_footer action
@@ -30,6 +26,7 @@
 			 * @hooked storefront_footer_widgets - 10
 			 * @hooked storefront_credit         - 20
 			 */
+			echo 'Loe ka meie e-poe <a href="http://rasmuslaane.ikt.khk.ee/wordpress5/e-poe-kasutustingimused/">kasutustingimusi</a>.';
 			do_action( 'storefront_footer' );
 			?>
 			
