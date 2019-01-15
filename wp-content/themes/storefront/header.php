dac814f69a0d6b672b5bb9aa66f8bee15be02508
RL.ee <rasmus.laane@khk.ee>
2019-01-15 11:56:47 +0200

    modified theme `Storefront` version 2.4.2

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -17,7 +17,7 @@
 <meta name="keywords" content="vabaaja tooted, telefonide kaitseümbrised, telefonide lisavarustus">
 <meta name="author" content="Rasmus Laane">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<!-- <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"> -->
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
 </head>
